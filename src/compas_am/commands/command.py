468c84c87c5900952d224546e58534903adcefe2
joburger <burger@arch.ethz.ch>
2020-04-21 15:56:39 +0200

    Added class for MachineData

--- src/compas_am/commands/command.py
+++ src/compas_am/commands/command.py
@@ -1,3 +1,8 @@
 class Command(object):
     def __init__(self):
         pass
+
+class MachineData(object):
+    def __init__(self):
+        # class for representing various fabrication machines (3D printers, robots etc.)
+        pass
