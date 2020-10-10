9e6e9a57395e178e825b413d01049dc74e6fa5c5
Joris Burger <burger@arch.ethz.ch>
2020-10-10 13:58:50 +0200

    Updated seams_smooth to only accept single-contoured objects

--- src/compas_slicer/functionality/seams_smooth.py
+++ src/compas_slicer/functionality/seams_smooth.py
@@ -23,23 +23,28 @@ def seams_smooth(slicer, smooth_distance):
     logger.info("Smoothing seams with a distance of %i mm" % smooth_distance)
 
     for layer in slicer.layers:
-        for path in layer.paths:  
-            pt0 = path.points[0]
-            # only points in the first half of a path should be evaluated
-            half_of_path = path.points[:int(len(path.points)/2)]
-            for point in half_of_path:
-                if distance_point_point(pt0, point) < smooth_distance:
-                    # remove points if within smooth_distance
-                    path.points.pop(0)
-                else: 
-                    # create new point at a distance of the 
-                    # 'smooth_distance' from the first point, 
-                    # so that all seams are of equal length
-                    vect = Vector.from_start_end(pt0, point)
-                    vect.unitize()
-                    new_pt = pt0 + (vect * smooth_distance)
-                    path.points.insert(0, new_pt)
-                    break
+        if len(layer.paths) == 1:
+            for path in layer.paths:  
+                pt0 = path.points[0]
+                # only points in the first half of a path should be evaluated
+                half_of_path = path.points[:int(len(path.points)/2)]
+                for point in half_of_path:
+                    if distance_point_point(pt0, point) < smooth_distance:
+                        # remove points if within smooth_distance
+                        path.points.pop(0)
+                    else: 
+                        # create new point at a distance of the 
+                        # 'smooth_distance' from the first point, 
+                        # so that all seams are of equal length
+                        vect = Vector.from_start_end(pt0, point)
+                        vect.unitize()
+                        new_pt = pt0 + (vect * smooth_distance)
+                        path.points.insert(0, new_pt)
+                        break
+        else:
+            logger.warning("Smooth seams only works for layers consisting out of a single path, paths were not changed, seam smoothing skipped")
+            break
+
 
 if __name__ == "__main__":
     pass
