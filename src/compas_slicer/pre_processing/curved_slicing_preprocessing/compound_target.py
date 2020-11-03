2e18c477ca2f9e3927113c2f2986c71c2d6acb3c
Ioanna Mitropoulou <iwan.na21@hotmail.com>
2020-11-03 13:56:27 +0100

    Changed topological sorting of split meshes so that it can accept more than one cut ids

--- src/compas_slicer/pre_processing/curved_slicing_preprocessing/compound_target.py
+++ src/compas_slicer/pre_processing/curved_slicing_preprocessing/compound_target.py
@@ -61,7 +61,7 @@ class CompoundTarget:
 
         self.L = None
 
-        self.offset = 0  # 16
+        self.offset = 21
         self.VN = len(list(self.mesh.vertices()))
 
         # targets connected components
