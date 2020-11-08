dcaeac65038f3b509921d36a814db66f43a34de9
Ioanna Mitropoulou <iwan.na21@hotmail.com>
2020-11-08 14:46:07 +0100

    Restoring safety printpoints

--- src/compas_slicer/print_organization/print_organization_utilities/safety_printpoints.py
+++ src/compas_slicer/print_organization/print_organization_utilities/safety_printpoints.py
@@ -29,10 +29,10 @@ def add_safety_printpoints(print_organizer, z_hop=20.0):
     pp_copy_dict = {}  # should not be altering the dict that we are iterating through > copy
 
     # get last layer key and path
-    # last_layer_key = 'layer_%d' % (len(pp_dict) - 1)
-    # last_path_key = 'path_%d' % (len(pp_dict[last_layer_key]) - 1)
-    # # check if there are multiple paths in the last layer
-    # multiple_paths_in_last_layer = False if len(pp_dict[last_layer_key]) == 1 else True
+    last_layer_key = 'layer_%d' % (len(pp_dict) - 1)
+    last_path_key = 'path_%d' % (len(pp_dict[last_layer_key]) - 1)
+    # check if there are multiple paths in the last layer
+    multiple_paths_in_last_layer = False if len(pp_dict[last_layer_key]) == 1 else True
 
     for layer_key in pp_dict:
         pp_copy_dict[layer_key] = {}
@@ -41,36 +41,34 @@ def add_safety_printpoints(print_organizer, z_hop=20.0):
             pp_copy_dict[layer_key][path_key] = []
 
             for i, printpoint in enumerate(pp_dict[layer_key][path_key]):
-                # Commenting out this code, will remove on next iteration.
-                # I believe that it doesnt's do anything more than what is done in lines 61-63.
-                #
-                # # if not the first point of the entire print
-                # if printpoint is not pp_dict['layer_0']['path_0'][0]:
-                #     # or the first point of last layer (except when there are multiple paths in last layer)
-                #     if printpoint is not pp_dict[last_layer_key][last_path_key][0] or multiple_paths_in_last_layer:
-                #         # check if the last point of a path is set to False
-                #         if i == 0 and not pp_dict[layer_key][path_key][-1].extruder_toggle:
-                #             # if False, add safety point
-                #             safety_printpoint = create_safety_printpoint(printpoint, z_hop, False)
-                #             pp_copy_dict[layer_key][path_key].append(safety_printpoint)
+
+                # if not the first point of the entire print
+                if printpoint is not pp_dict['layer_0']['path_0'][0]:
+                    # or the first point of last layer (except when there are multiple paths in last layer)
+                    if printpoint is not pp_dict[last_layer_key][last_path_key][0] or multiple_paths_in_last_layer:
+                        # check if the last point of a path is set to False
+                        if i == 0 and not pp_dict[layer_key][path_key][-1].extruder_toggle:
+                            # if False, add safety point
+                            safety_printpoint = add_safety_printpoint(printpoint, z_hop, False)
+                            pp_copy_dict[layer_key][path_key].append(safety_printpoint)
 
                 #  regular printing points
                 pp_copy_dict[layer_key][path_key].append(printpoint)
 
                 #  adds a safety point after every printpoint that has extruder_toggle = False
                 if printpoint.extruder_toggle is False:
-                    safety_printpoint = create_safety_printpoint(printpoint, z_hop, False)
+                    safety_printpoint = add_safety_printpoint(printpoint, z_hop, False)
                     pp_copy_dict[layer_key][path_key].append(safety_printpoint)
 
     #  insert a safety print point at the beginning
-    safety_printpoint = create_safety_printpoint(pp_dict['layer_0']['path_0'][0], z_hop, False)
+    safety_printpoint = add_safety_printpoint(pp_dict['layer_0']['path_0'][0], z_hop, False)
     pp_copy_dict['layer_0']['path_0'].insert(0, safety_printpoint)
 
     #  the safety printpoint has already been added at the end since the last printpoint extruder_toggle_type is False
     print_organizer.printpoints_dict = pp_copy_dict
 
 
-def create_safety_printpoint(printpoint, z_hop, extruder_toggle):
+def add_safety_printpoint(printpoint, z_hop, extruder_toggle):
     """
 
     Parameters
