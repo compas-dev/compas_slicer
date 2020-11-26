eea94b6c36293b0143251284eb6c547509039704
Ioanna Mitropoulou <iwan.na21@hotmail.com>
2020-11-26 14:55:55 +0100

    Fixed sort paths per vertical segment and tested brim + extruder toggle with it

--- src/compas_slicer/post_processing/sort_paths_per_vertical_segment.py
+++ src/compas_slicer/post_processing/sort_paths_per_vertical_segment.py
@@ -7,11 +7,11 @@ import logging
 
 logger = logging.getLogger('logger')
 
-__all__ = ['sort_per_segment',
+__all__ = ['sort_per_vertical_segment',
            'get_segments_centroids_list']
 
 
-def sort_per_segment(slicer, max_layers_per_segment, threshold):
+def sort_per_vertical_segment(slicer, dist_threshold=30.0, max_layers_per_segment=None, ):
     """Sorts in vertical vertical_layers_print_data the contours that are stored in the horizontal layers.
     This is done by grouping the centroids of the paths based on proximity.
 
@@ -19,7 +19,7 @@ def sort_per_segment(slicer, max_layers_per_segment, threshold):
     ----------
     slicer: :class:`compas_slicer.slicers.BaseSlicer`
         An instance of one of the compas_slicer.slicers classes.
-    threshold: float
+    dist_threshold: float
         The maximum get_distance that the centroids of two successive paths can have to belong in the same group
         Recommended value, slightly bigger than the layer height
     max_layers_per_segment: int
@@ -29,19 +29,19 @@ def sort_per_segment(slicer, max_layers_per_segment, threshold):
     """
     logger.info("Sorting per segment")
 
-    segments = [VerticalLayer(id=0)]  # vertical_layers_print_data that contain isocurves
+    vertical_segments = [VerticalLayer(id=0)]  # vertical_layers_print_data that contain isocurves
     for layer in slicer.layers:
         for path in layer.paths:
             current_segment = None
 
             #  Find an eligible segment for contour (called current_segment)
-            if len(segments[0].paths) == 0:  # first contour
-                current_segment = segments[0]
+            if len(vertical_segments[0].paths) == 0:  # first contour
+                current_segment = vertical_segments[0]
             else:  # find the candidate segment for new isocurve
                 contour_centroid = list(np.average(np.array(path.points), axis=0))
-                other_centroids = get_segments_centroids_list(segments)
-                candidate_segment = segments[utils.get_closest_pt_index(contour_centroid, other_centroids)]
-                if distance_point_point(candidate_segment.head_centroid, contour_centroid) < threshold:
+                other_centroids = get_segments_centroids_list(vertical_segments)
+                candidate_segment = vertical_segments[utils.get_closest_pt_index(contour_centroid, other_centroids)]
+                if distance_point_point(candidate_segment.head_centroid, contour_centroid) < dist_threshold:
                     if max_layers_per_segment:
                         if len(candidate_segment.paths) < max_layers_per_segment:
                             current_segment = candidate_segment
@@ -49,14 +49,14 @@ def sort_per_segment(slicer, max_layers_per_segment, threshold):
                         current_segment = candidate_segment
 
                 if not current_segment:  # then create new segment
-                    current_segment = VerticalLayer(id=segments[-1].id + 1)
-                    segments.append(current_segment)
+                    current_segment = VerticalLayer(id=vertical_segments[-1].id + 1)
+                    vertical_segments.append(current_segment)
 
             #  Assign contour to current segment
             current_segment.append_(path)
 
-    logger.info("Number of vertical_layers_print_data : %d" % len(segments))
-    slicer.print_paths = segments
+    logger.info("Number of vertical_layers_print_data : %d" % len(vertical_segments))
+    slicer.layers = vertical_segments
 
 
 def get_segments_centroids_list(segments):
