b7cc47b37a82c63dfaae82e43ec2691733a2a340
Andrei <stratocaster47@yahoo.com>
2021-01-05 20:46:11 +0100

    Added get Parameter section

--- src/compas_slicer/print_organization/print_organization_utilities/gcode.py
+++ src/compas_slicer/print_organization/print_organization_utilities/gcode.py
@@ -27,12 +27,41 @@ def create_gcode_text(printpoints_dict, parameters):
     logger.info('Generating gcode')
     gcode = ''
 
-    # get all the necessary parameters:
-    z_hop = get_param(parameters, key='z_hop', defaults_type='gcode')
-    extruder_temp = get_param(parameters, key='extruder_temperature', defaults_type='gcode')
-    bed_temp = get_param(parameters, key='bed_temperature', defaults_type='gcode')
+    ############################################
+    # get all the necessary parameters:    
+    # Physical parameters
+    delta = get_param(parameters, key='delta', defaults_type='gcode') # boolean for delta printers
+    nozzle_diameter = get_param(parameters, key='nozzle_diameter', defaults_type='gcode')  #in mm
+    filament diameter = get_param(parameters, key='filament diameter', defaults_type='gcode')  #in mm
+    
+    # Dimensional parameters
+    layer_width = get_param(parameters, key='layer_width', defaults_type='gcode')  #in mm
+    
+    # Temperature parameters
+    extruder_temperature = get_param(parameters, key='extruder_temperature', defaults_type='gcode')  #in °C
+    bed_temperature = get_param(parameters, key='bed_temperature', defaults_type='gcode')  #in °C
+    fan_speed = get_param(parameters, key='bed_temperature', defaults_type='gcode')  #0-255
+    fan_start_z = get_param(parameters, key='fan_start_z', defaults_type='gcode')  #in mm
+    
+    # Movement parameters
+    feedrate = get_param(parameters, key='feedrate', defaults_type='gcode')  #in mm/s
+    feedrate_travel = get_param(parameters, key='feedrate_travel', defaults_type='gcode')  #in mm/s
+    feedrate_low = get_param(parameters, key='feedrate_low', defaults_type='gcode')  #in mm/s
+    feedrate_retraction = get_param(parameters, key='feedrate_retraction', defaults_type='gcode')  #in mm/s   
+    # acceleration = get_param(parameters, key='acceleration', defaults_type='gcode')  #in mm/s²   
+    # jerk = get_param(parameters, key='jerk', defaults_type='gcode')  #in mm/s
+    
+    # Retraction
+    z_hop = get_param(parameters, key='z_hop', defaults_type='gcode')  #in mm
+    retraction_length = get_param(parameters, key='retraction_length', defaults_type='gcode')  #in mm
+    retraction_min_travel = get_param(parameters, key='retraction_min_travel', defaults_type='gcode')  #in mm
+    ############################################ / get parmeters
+    
+    
     # ....
 
+    
+    
     # Iterate through the printpoints_dict and add information to the gcode str
     for layer_key in printpoints_dict:
         for path_key in printpoints_dict[layer_key]:
@@ -40,6 +69,5 @@ def create_gcode_text(printpoints_dict, parameters):
             gcode += 'this is a command %.4f %.4f %.4f \n' % (z_hop, extruder_temp, bed_temp)  # just remove this line
 
             # .... gcode += 'command'
-    # Thanks!
 
     return gcode
