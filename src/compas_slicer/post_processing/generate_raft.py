e86f6272787842b9f5a1e661b05b409c840e3ac9
Joris Burger <burger@arch.ethz.ch>
2021-02-09 18:34:13 +0100

    Fixed some bugs in raft

--- src/compas_slicer/post_processing/generate_raft.py
+++ src/compas_slicer/post_processing/generate_raft.py
@@ -1,8 +1,4 @@
 import logging
-from compas.geometry._core import distance
-
-import pyclipper
-from pyclipper import scale_from_clipper, scale_to_clipper
 
 import compas_slicer
 from compas_slicer.geometry import Layer
@@ -12,14 +8,17 @@ from compas.geometry import Point
 from compas.geometry import bounding_box_xy
 from compas.geometry import offset_polygon
 
-from compas_slicer.post_processing import seams_align
-
 logger = logging.getLogger('logger')
 
 __all__ = ['generate_raft']
 
 
-def generate_raft(slicer, raft_offset=10, distance_between_lines=10, direction="x_axis"):
+def generate_raft(slicer,
+                  raft_offset=10,
+                  distance_between_paths=10,
+                  direction="x_axis",
+                  raft_layers=1,
+                  raft_layer_height="default"):
     """Creates a raft.
 
     Parameters
@@ -27,13 +26,21 @@ def generate_raft(slicer, raft_offset=10, distance_between_lines=10, direction="
     slicer: :class:`compas_slicer.slicers.BaseSlicer`
         An instance of one of the compas_slicer.slicers.BaseSlicer classes.
     raft_offset: float
-        Distance (in mm) that the raft should be offsetted from the first layer.
-    distance_between_lines: float
-        Distance (in mm) between the printed lines of the raft.
+        Distance (in mm) that the raft should be offsetted from the first layer. Defaults to 10mm
+    distance_between_paths: float
+        Distance (in mm) between the printed lines of the raft. Defaults to 10mm
     direction: str
         x_axis: Aligns the raft with the x axis
         y_axis: Aligns the raft with the y_axis
+    raft_layers: int
+        Number of raft layers to add. Defaults to 1
+    raft_layer_height: float
+        Layer height of the raft layers. Defaults to same value as used in the slicer.
     """
+
+    # check if a raft_layer_height is specified, if not, use the slicer.layer_height value
+    if raft_layer_height == "default":
+        raft_layer_height = slicer.layer_height
 
     logger.info("Generating raft")
 
@@ -41,12 +48,10 @@ def generate_raft(slicer, raft_offset=10, distance_between_lines=10, direction="
     if isinstance(slicer.layers[0], compas_slicer.geometry.VerticalLayer):  # Vertical layers
         # then find all paths that lie on the print platform and make them brim.
         paths_to_offset, _ = slicer.find_vertical_layers_with_first_path_on_base()
-        has_vertical_layers = True
 
     else:  # Horizontal layers
         # then replace the first layer with a raft layer.
         paths_to_offset = slicer.layers[0].paths
-        has_vertical_layers = False
 
     # get flat lists of points in bottom layer
     all_pts = []
@@ -54,45 +59,49 @@ def generate_raft(slicer, raft_offset=10, distance_between_lines=10, direction="
         for pt in path.points:
             all_pts.append(pt)
 
-    # get xy bounding box and create offset
+    # get xy bounding box of bottom layer and create offset
     bb_xy = bounding_box_xy(all_pts)
     bb_xy_offset = offset_polygon(bb_xy, -raft_offset)
 
-    # calculate x range, y range and number of steps
+    # calculate x range, y range, and number of steps
     x_range = abs(bb_xy_offset[0][0] - bb_xy_offset[1][0])
     y_range = abs(bb_xy_offset[0][1] - bb_xy_offset[3][1])
     if direction == "y_axis":
-        no_of_steps = int(x_range/distance_between_lines)
+        no_of_steps = int(x_range/distance_between_paths)
     elif direction == "x_axis":
-        no_of_steps = int(y_range/distance_between_lines)
+        no_of_steps = int(y_range/distance_between_paths)
 
-    # get raft start point
-    raft_start_pt = bb_xy_offset[0]
+    # get raft start point with correct z height
+    raft_start_pt = Point(bb_xy_offset[0][0], bb_xy_offset[0][1], slicer.layers[0].paths[0].points[0][2])
 
-    raft_points = []
-    for i in range(no_of_steps+1):
-        # create raft points depending on the chosen direction
-        if direction == "y_axis":
-            raft_pt1 = Point(raft_start_pt[0] + i*distance_between_lines, raft_start_pt[1], raft_start_pt[2])
-            raft_pt2 = Point(raft_start_pt[0] + i*distance_between_lines, raft_start_pt[1] + y_range, raft_start_pt[2])
-        elif direction == "x_axis":
-            raft_pt1 = Point(raft_start_pt[0], raft_start_pt[1] + i*distance_between_lines, raft_start_pt[2])
-            raft_pt2 = Point(raft_start_pt[0] + x_range, raft_start_pt[1] + i*distance_between_lines, raft_start_pt[2])
-        # append to list alternating
-        if i % 2 == 0:
-            raft_points.extend((raft_pt1, raft_pt2))
-        else:
-            raft_points.extend((raft_pt2, raft_pt1))
+    # move all points in the slicer up so that raft layers can be inserted
+    for i, layer in enumerate(slicer.layers):
+        for j, path in enumerate(layer.paths):
+            for k, pt in enumerate(path.points):
+                slicer.layers[i].paths[j].points[k] = Point(pt[0], pt[1], pt[2] + (raft_layers-1)*raft_layer_height)
 
-    # create raft layer
-    raft_layer = Layer([Path(raft_points, is_closed=False)])
+    for i in range(raft_layers):
+        raft_points = []
 
-    # add the raft layer to the slicer
-    if not has_vertical_layers:
-        slicer.layers[0] = raft_layer  # replace first layer
-    else:
-        slicer.layers.insert(0, raft_layer)  # insert brim layer as the first layer of the slicer
+        for j in range(no_of_steps+1):
+            # create raft points depending on the chosen direction
+            if direction == "y_axis":
+                raft_pt1 = Point(raft_start_pt[0] + j*distance_between_paths, raft_start_pt[1], raft_start_pt[2] + i*raft_layer_height)
+                raft_pt2 = Point(raft_start_pt[0] + j*distance_between_paths, raft_start_pt[1] + y_range, raft_start_pt[2] + i*raft_layer_height)
+            elif direction == "x_axis":
+                raft_pt1 = Point(raft_start_pt[0], raft_start_pt[1] + j*distance_between_paths, raft_start_pt[2] + i*raft_layer_height)
+                raft_pt2 = Point(raft_start_pt[0] + x_range, raft_start_pt[1] + j*distance_between_paths, raft_start_pt[2] + i*raft_layer_height)
 
+            # append to list alternating
+            if j % 2 == 0:
+                raft_points.extend((raft_pt1, raft_pt2))
+            else:
+                raft_points.extend((raft_pt2, raft_pt1))
+
+        # create raft layer
+        raft_layer = Layer([Path(raft_points, is_closed=False)])
+        # insert raft layer in the correct position into the slicer
+        slicer.layers.insert(i, raft_layer)
 
 if __name__ == "__main__":
     pass
