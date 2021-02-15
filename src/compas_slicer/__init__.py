8b2318b317f0d19559481042a342bebb9d930b68
Joris Burger <burger@arch.ethz.ch>
2021-02-15 18:52:56 +0100

    Bump version to 0.1.3

--- src/compas_slicer/__init__.py
+++ src/compas_slicer/__init__.py
@@ -29,7 +29,7 @@ __author__ = ['Ioanna Mitropoulou and Joris Burger']
 __copyright__ = 'Copyright 2020 ETH Zurich'
 __license__ = 'MIT License'
 __email__ = 'mitropoulou@arch.ethz.ch'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 
 HERE = os.path.dirname(__file__)
