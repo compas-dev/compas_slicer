6d9dc772ad61616c067c486bc2f987297a0f0445
Gonzalo Casas <casas@arch.ethz.ch>
2021-02-15 16:55:42 +0100

    Update metadata (#113)

--- src/compas_slicer/__init__.py
+++ src/compas_slicer/__init__.py
@@ -26,7 +26,7 @@ import compas
 
 
 __author__ = ['Ioanna Mitropoulou and Joris Burger']
-__copyright__ = 'copyright 2020 ETH'
+__copyright__ = 'Copyright 2020 ETH Zurich'
 __license__ = 'MIT License'
 __email__ = 'mitropoulou@arch.ethz.ch'
 __version__ = '0.1.2'
