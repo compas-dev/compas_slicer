1449f60b30f67b2be6ab7639be4ff24705beb6f3
Joris Burger <burger@arch.ethz.ch>
2021-02-24 14:47:07 +0100

    Revert "Bump version to 0.1.3"
    This reverts commit 8b7ecc4ad7c478be6489069e035ba78a0f2a381c.

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ project = 'COMPAS SLICER'
 year = '2020'
 author = 'Digital Building Technologies, Gramazio Kohler Research'
 copyright = '{0}, {1}'.format(year, author)
-version = release = 'version = release = '0.1.3''
+version = release = '0.1.3'
 
 pygments_style = 'sphinx'
 show_authors = True
