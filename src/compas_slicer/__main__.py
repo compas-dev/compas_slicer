d0996d59fcc7c92caf60c25ae15f3d9fec911b54
Gonzalo Casas <casas@arch.ethz.ch>
2021-03-30 10:35:49 +0200

    Update installation to use conda

---
+++ src/compas_slicer/__main__.py
@@ -0,0 +1,7 @@
+import compas
+import compas_slicer
+
+if __name__ == '__main__':
+    print('COMPAS: {}'.format(compas.__version__))
+    print('COMPAS Slicer: {}'.format(compas_slicer.__version__))
+    print('Awesome! Your installation worked! :)')
