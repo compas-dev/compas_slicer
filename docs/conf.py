f67753fd8706b992d3253fe7b499ec6a0539d4a3
Joris Burger <burger@arch.ethz.ch>
2021-04-06 15:18:29 +0200

    Bump version to 0.3.3

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ project = 'COMPAS SLICER'
 year = '2020'
 author = 'Digital Building Technologies, Gramazio Kohler Research'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.3.2'
+version = release = '0.3.3'
 
 pygments_style = 'sphinx'
 show_authors = True
