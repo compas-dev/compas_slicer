5dc73acfa254ba7fd614b6f76298311a55bebe12
Joris Burger <burger@arch.ethz.ch>
2021-10-05 16:22:59 +0200

    Bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ optional_requirements = {}
 
 setup(
     name="compas_slicer",
-    version="0.3.5",
+    version="0.4.0",
     description="Slicing package for FDM 3D Printing with COMPAS",
     long_description=long_description,
     long_description_content_type="text/markdown",
