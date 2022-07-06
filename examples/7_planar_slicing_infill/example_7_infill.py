e780f4905bacedac1dff0b45c661f90a1c703ba7
patbed <bedarf@arch.ethz.ch>
2022-07-06 16:46:57 +0200

    update fermat spiral

--- examples/7_planar_slicing_infill/example_7_infill.py
+++ examples/7_planar_slicing_infill/example_7_infill.py
@@ -14,7 +14,7 @@ from compas_slicer.print_organization import add_safety_printpoints
 from compas_slicer.print_organization import set_linear_velocity_constant
 from compas_slicer.print_organization import set_blend_radius
 from compas_slicer.utilities import save_to_json, load_from_json
-# from compas_view2 import app
+from compas_view2 import app
 
 from compas.datastructures import Mesh
 from compas.geometry import Point
@@ -108,10 +108,10 @@ def main():
     # ==========================================================================
     # Initializes the compas_viewer and visualizes results
     # ==========================================================================
-    # viewer = app.App(width=1600, height=1000)
-    # slicer.visualize_on_viewer(viewer, visualize_mesh=False, visualize_paths=True)
-    # print_organizer.visualize_on_viewer(viewer, visualize_printpoints=True)
-    # viewer.show()
+    viewer = app.App(width=1600, height=1000)
+    slicer.visualize_on_viewer(viewer, visualize_mesh=False, visualize_paths=True)
+    print_organizer.visualize_on_viewer(viewer, visualize_printpoints=True)
+    viewer.show()
 
     end_time = time.time()
     print("Total elapsed time", round(end_time - start_time, 2), "seconds")
